6d856e886de2decba203725b8109981730bbbfcb
Lorenzo Iacovone <99271423+javoxpl@users.noreply.github.com>
2022-07-07 15:59:02 +0200

    errore da sistemare

--- index.php
+++ index.php
@@ -5,15 +5,22 @@ Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
 
 <?php
+$termini = $_GET["termini"];
 $paragrafo = "ciao forse stai leggendo questo testo, perchè è il testo più bello che si possa trovare tra tutti quelli che diconon di pubblicare un testo, 
 senza però sapere che non sono realmente capaci di scrivere un testo";
 
 $lunghezzaParagrafo = strlen($paragrafo);
 
+
 echo "la lunghezza di questo testo è : " . "=" . strlen($paragrafo);
+
+//if(strpos($paragrafo, "testo") !== false ) {
+    $paragrafo = str_replace("testo", "***", $paragrafo);
+};
 
 ?>
 
 
 <h1> questo è il paragrafo : <?php echo $paragrafo ?></h1>
 <p>  questa è la lunghezza del paragrafo <strong><?php echo $lunghezzaParagrafo ?></strong> </p>
+<p><?php echo $paragrafo ?></p>
