f51eb181667223ec7778fb9454c8db49eed6136d
Lorenzo Iacovone <99271423+javoxpl@users.noreply.github.com>
2022-07-07 16:09:21 +0200

    fixed?

--- index.php
+++ index.php
@@ -5,17 +5,16 @@ Una parola da censurare viene passata dall'utente tramite parametro GET.
 Stampare di nuovo il paragrafo e la sua lunghezza, dopo aver sostituito con tre asterischi (***) tutte le occorrenze della parola da censurare.
 
 <?php
-$termini = $_GET["termini"];
 $paragrafo = "ciao forse stai leggendo questo testo, perchè è il testo più bello che si possa trovare tra tutti quelli che diconon di pubblicare un testo, 
 senza però sapere che non sono realmente capaci di scrivere un testo";
 
 $lunghezzaParagrafo = strlen($paragrafo);
-
+$paragrafoCorretto = $paragrafo;
 
 echo "la lunghezza di questo testo è : " . "=" . strlen($paragrafo);
 
-//if(strpos($paragrafo, "testo") !== false ) {
-    $paragrafo = str_replace("testo", "***", $paragrafo);
+if(strpos($paragrafo, "testo") !== false ) {
+    $paragrafoCorretto = str_replace("testo", "***", $paragrafo);
 };
 
 ?>
@@ -23,4 +22,4 @@ echo "la lunghezza di questo testo è : " . "=" . strlen($paragrafo);
 
 <h1> questo è il paragrafo : <?php echo $paragrafo ?></h1>
 <p>  questa è la lunghezza del paragrafo <strong><?php echo $lunghezzaParagrafo ?></strong> </p>
-<p><?php echo $paragrafo ?></p>
+<p><?php echo $paragrafoCorretto ?></p>
